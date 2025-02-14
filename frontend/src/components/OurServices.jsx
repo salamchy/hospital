0b9517f4fefa9845f6837c95ca839f39aa543ffa
salam <chaudharysm00@gmail.com>
2025-02-14 19:00:02 +0545

    our service

---
+++ frontend/src/components/OurServices.jsx
@@ -0,0 +1,12 @@
+import MedicalUI from "./MedicalUi"
+import Title from "./Title"
+
+const OurServices = () => {
+  return (
+    <div className="md:my-16 my-8">
+      <Title title="care you can believe in" subtitle="Our Services" />
+      <MedicalUI />
+    </div>
+  )
+}
+export default OurServices
